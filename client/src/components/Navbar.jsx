6f221d7e4613a340ed4073bd7d9fa2585cc3d49f
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-12-03 20:09:58 +0530

    styling init

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,49 +1,68 @@
 import React from "react";
 import { FiYoutube } from "react-icons/fi";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 import { useAppContext } from "../context/app";
 
 const Navbar = () => {
   const { theme, setTheme } = useAppContext();
 
   return (
-    <Container>
-      <button
-        onClick={() =>
-          setTheme((theme) => (theme === "light" ? "dark" : "light"))
-        }
-      >
-        Change Theme
-      </button>
-      <div className="nav__logo">
-        <div className="nav__logo-container">
-          <FiYoutube size={42} />
-          YouTube Playlist Length
+    <Container theme={theme}>
+      <div className="nav__container">
+        <div className="nav__logo">
+          <Link to="/">
+            <div className="nav__logo-container">
+              <FiYoutube size={42} />
+              YouTube Playlist Length
+            </div>
+          </Link>
         </div>
+
+        <button
+          onClick={() =>
+            setTheme((theme) => (theme === "light" ? "dark" : "light"))
+          }
+        >
+          Change Theme
+        </button>
       </div>
     </Container>
   );
 };
 
-const Container = styled.div`
-  width: 100%;
-  height: 70px;
-  padding: 8px 16px;
-  background-color: olive;
+const Container = styled.nav`
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  position: sticky;
+  top: 0;
+  background-color: blue;
+  max-width: 900px;
+  height: 80px;
+  padding: 8px 16px;
+  margin: auto;
+  z-index: 50;
+  width: 100%;
 
-  & .nav__logo {
-    height: 100%;
+  & .nav__container {
     display: flex;
+    justify-content: space-between;
     align-items: center;
+    width: 90%;
 
-    & .nav__logo-container {
-      font-size: 48px;
-      line-height: 1;
+    & .nav__logo {
+      height: 100%;
+      width: 80%;
       display: flex;
       align-items: center;
+
+      & .nav__logo-container {
+        font-size: 20px;
+        line-height: 1;
+        display: flex;
+        align-items: center;
+      }
     }
   }
 `;
